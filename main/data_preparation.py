16225a7818ccc163e7c7361ca73b4487c8c37f32
nbrosson <Nolwen.Brosson@enablon.com>
2020-11-17 20:11:13 +0100

    logic implemented

--- main/data_preparation.py
+++ main/data_preparation.py
@@ -4,9 +4,31 @@ detector = RNNDetector()
 
 class DataPreparation:
 
-    def __init__(self):
-        detector = RNNDetector()
+    def __init__(self, expected_dim):
+        self.detector = RNNDetector()
+        self.expected_dim = expected_dim
+        self.global_score = None
+        self.local_score = None
 
     def predict_wav(self, wav_audio_path):
-        global_score, local_score = detector.predict_on_wav(wav_audio_path)
-        return global_score, local_score
+        self.global_score, self.local_score = self.detector.predict_on_wav(wav_audio_path)
+        redimension_local_score = self.reshape_prediction(self.local_score)
+        return redimension_local_score
+
+    def reshape_prediction(self):
+        """
+        If dimension is too small for the model, we will add some zeros to fill the blanks
+        If dimension is too high, we will keep the most interesting portion.
+        :return:
+        """
+        if self.local_score.shape[0] < self.expected_dim:
+            return self._reshape_small_array()
+        elif self.local_score.shape[0] > self.expected_dim:
+            return self._reshape_big_array()
+        return self.local_score
+
+    def _reshape_small_array(self, local_score):
+        pass
+
+    def _reshape_big_array(self, local_score):
+        pass
